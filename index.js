d00c53e8ea6a2dbf6752e01b5d388e7ed2dccfb6
gladishd <gladish.dean@gmail.com>
2023-02-04 00:01:19 -0600

    changes allowedDomains to whitelist

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(
     cors({
         origin: function (origin, callback) {
             if (!origin) return callback(null, true);
-            if (allowedDomains.indexOf(origin) === -1) {
+            if (whitelist.indexOf(origin) === -1) {
                 var msg = `The CORS policy for this site ${origin} does not allow access from the specified Origin.`;
                 return callback(new Error(msg), false);
             }
