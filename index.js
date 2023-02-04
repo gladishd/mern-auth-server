dc0af66807cbb33e803e13d0ff8e09aeafa66c70
gladishd <gladish.dean@gmail.com>
2023-02-03 23:36:13 -0600

    testing out access control allow origin

--- index.js
+++ index.js
@@ -23,6 +23,44 @@ app.use(cors({
     credentials: true,
 }));
 
+const whitelist = ['https://localhost:3000', 'http://localhost:3000', 'https://mangrove-research.vercel.app/']
+
+app.use(function (req, res, next) {
+    let currentOrigin = req.headers.origin;
+    if (whitelist.indexOf(currentOrigin) > -1) {
+        origin = theOrigin;
+    } else {
+        origin = 'https://mangrove-research.vercel.app/';
+    }
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', "GET, POST, OPTIONS, PUT, DELETE");
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    if (req.method == 'OPTIONS' &&
+        req.rawHeaders.includes('Origin') &&
+        req.rawHeaders.includes('Access-Control-Request-Method')) {
+        res.status(204).end();
+        return;
+    }
+    next()
+})
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedDomains.indexOf(origin) === -1) {
+                var msg = `The CORS policy for this site ${origin} does not allow access from the specified Origin.`;
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        }
+    })
+)
+
 //remove the warning
 mongoose.set('strictQuery', true)
 
